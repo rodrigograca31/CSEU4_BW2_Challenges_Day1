7942d2aad26f0146cdfd243c875226b046856d4d
Rodrigo Graça <rodrigograca31@gmail.com>
2020-05-25 19:04:15 +0100

    feat: ✨ contains dupe

--- contains-dupes/contains_dupe.py
+++ contains-dupes/contains_dupe.py
@@ -1,2 +1,24 @@
-def containsDuplicate(num):
-    pass
+def containsDuplicate(numbers):
+
+    # Approach 1
+    unique = set()
+
+    for num in numbers:
+        if num in unique:
+            return True
+        else:
+            unique.add(num)
+
+    return False
+
+    # Approach 2
+    numbersSet = set(numbers)
+
+    if len(numbers) != len(numbersSet):
+        return True
+    else:
+        return False
+
+
+result = containsDuplicate([9, 34, 67, 23, 3, 2, 3, 6])
+print(result)
